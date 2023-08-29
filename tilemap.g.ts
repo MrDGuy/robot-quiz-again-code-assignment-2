cbd7b5f0f68d35596915c6a7f1a44bc1f84d89a2
MrDGuy <84729967+MrDGuy@users.noreply.github.com>
2023-08-28 21:47:00 -0400

    Update tilemap.g.ts

--- tilemap.g.ts
+++ tilemap.g.ts
@@ -37,11 +37,11 @@ namespace myTiles {
             case "level1":
             case "level1":return tiles.createTilemap(hex`0a0007000304040b04040b04040509020f0211020f021106090211021102110211060e02110211021102110d09021102110211021106090111020f0211021006080a0a0c0a0a0c0a0a07`, img`
 2 2 2 2 2 2 2 2 2 2 
-2 . . . . . . . . 2 
-2 . . . . . . . . 2 
-2 . . . . . . . . 2 
-2 . . . . . . . . 2 
-2 . . . . . . . . 2 
+2 . . . 2 . . . 2 2 
+2 . 2 . 2 . 2 . 2 2 
+2 . 2 . 2 . 2 . 2 2 
+2 . 2 . 2 . 2 . 2 2 
+2 . 2 . . . 2 . . 2 
 2 2 2 2 2 2 2 2 2 2 
 `, [myTiles.transparency16,myTiles.tile1,myTiles.tile3,sprites.dungeon.greenOuterNorthWest,sprites.dungeon.greenOuterNorth0,sprites.dungeon.greenOuterNorthEast,sprites.dungeon.greenOuterEast0,sprites.dungeon.greenOuterSouthWest,sprites.dungeon.greenOuterSouthEast,sprites.dungeon.greenOuterWest0,sprites.dungeon.greenOuterSouth1,sprites.dungeon.greenOuterNorth2,sprites.dungeon.greenOuterSouth2,sprites.dungeon.greenOuterEast2,sprites.dungeon.greenOuterWest2,myTiles.tile4,myTiles.tile2,myTiles.tile5], TileScale.Sixteen);
         }
